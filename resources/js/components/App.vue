784ce8ed44dcfa8aaac4487ec0cfc73d451bc6f4
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-01-24 20:18:04 +0100

    Install vue as the frontend framework.

---
+++ resources/js/components/App.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        What's up dude?
+    </h1>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/welcome', function () {
-    return view('welcome');
+    return view('app');
 });
