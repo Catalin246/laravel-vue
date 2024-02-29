63ca4562d753adeb052687f241076ca9d308521d
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-02-29 14:49:53 +0100

    Add 'auth' to the endpoints for authentication.

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -15,8 +14,10 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::post('refresh', [AuthController::class, 'refresh']);
-Route::post('logout', [AuthController::class, 'logout']);
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
 
