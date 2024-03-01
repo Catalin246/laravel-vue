5ec2fd33f1f19f9528884632e2b663ef81f9a7ce
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-03-01 17:21:02 +0100

    Basic request to the api from the vue frontend.

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -5,11 +5,28 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+      items: {},
+    };
+  },
+  methods: {
+    async getItems() {
+      const { data } = await axios.get("http://localhost:8000/api/items");
+      this.items = data;
+      console.log(this.items);
+    },
+  },
+  beforeMount() {
+    this.getItems();
+  },
 }
 </script>
 
